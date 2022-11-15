a0b4a62e1d0b93873ef76c7d2f5d5ac9ab64dd82
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2022-11-15 23:13:55 +0100

    Added screen sizes brakpoints for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "14440px",
+    },
     colors: {
       priGreen: "#7ea33e",
       priGray: "#858f9a",
@@ -14,13 +18,13 @@ module.exports = {
       secBlue: "209bd0",
       pureBlue: "#213347",
       priBrown: "#707070",
-      pureWhite: "#ffffff"
+      pureWhite: "#ffffff",
     },
     extend: {
       boxShadow: {
-        "3xl": "0px 5px 15px rgba(0, 0, 0, 0.35) "
-      }
+        "3xl": "0px 5px 15px rgba(0, 0, 0, 0.35) ",
+      },
     },
   },
   plugins: [],
-}
+};
