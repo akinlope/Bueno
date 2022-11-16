9c31eab094448ee8c9f0523d8aff2b95af603e5c
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2022-11-16 14:54:54 +0100

    pushing all out once

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import Home from "./Pages/Home";
 import Navbar from "./Navbar";
+import Menu from "./Pages/Menu";
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route 
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
-    </div>
-  );
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/menu" element={<Menu />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
