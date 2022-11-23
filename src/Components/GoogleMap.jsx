b9651274fa62461b9cafca8c4113970e92aee9c4
akinlope <akinrimisitt@gmail.com>
2022-11-23 13:38:54 +0100

    map added to the home page

---
+++ src/Components/GoogleMap.jsx
@@ -0,0 +1,19 @@
+const GoogleMap = () => {
+  return (
+    <div>
+      <iframe
+        title="for google map"
+        width="252"
+        height="200"
+        id="gmap_canvas"
+        src="https://maps.google.com/maps?q=ikoyi%20lagos&t=&z=17&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default GoogleMap;
